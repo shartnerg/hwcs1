73943ee885c6e01109ee9191363d4d81a2ad77cc
George Shartner <shartnerg@gmail.com>
2023-02-02 02:15:02 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("First number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n>m)
+{
+    Console.Write($"max: {n} ");
+    Console.Write($"min: {m} ");
+}
+else if (n<m)
+{
+    Console.Write($"max: {m} ");
+    Console.Write($"min: {n} ");
+}
+else if (n==m)
+{
+    Console.Write("numbers are equal");
+}
